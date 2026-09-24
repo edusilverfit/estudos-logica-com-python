d106ede4ab906911988a5f04e6da7a4cd7af7a11
Eduardo Silveira <eduardoasm1@gmail.com>
2026-09-24 22:16:43 +0000

    Novas sugestões

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,2 +1,2 @@
-# Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+# Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings..
 
